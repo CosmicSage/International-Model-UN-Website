ad1eb87356a1cef23d1a5f79ed41092417a3b82b
CosmicSage <prabhakarmishra9@yahoo.com>
2019-12-23 12:18:15 +0530

    Updates

---
+++ precache-manifest.75254d245945c91626601ec4e4cdc898.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee82ac06a67bdff7f50592b829bd553",
+    "url": "/International-Model-UN-Website/index.html"
+  },
+  {
+    "revision": "76eb7552d7c84558187a",
+    "url": "/International-Model-UN-Website/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "73c2859d3fc5cea05cf8",
+    "url": "/International-Model-UN-Website/static/css/main.274c1071.chunk.css"
+  },
+  {
+    "revision": "76eb7552d7c84558187a",
+    "url": "/International-Model-UN-Website/static/js/2.8b864c58.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/International-Model-UN-Website/static/js/2.8b864c58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "73c2859d3fc5cea05cf8",
+    "url": "/International-Model-UN-Website/static/js/main.25745710.chunk.js"
+  },
+  {
+    "revision": "e61a7906e53a5d1405a1",
+    "url": "/International-Model-UN-Website/static/js/runtime-main.87ef2298.js"
+  },
+  {
+    "revision": "7167ef84ea9a839f1d0105f4a56da4c2",
+    "url": "/International-Model-UN-Website/static/media/nasa-53884-unsplash (1).7167ef84.png"
+  },
+  {
+    "revision": "ffa91c95f945a7daf8947f88e3d2a3a5",
+    "url": "/International-Model-UN-Website/static/media/seattle.ffa91c95.png"
+  },
+  {
+    "revision": "820e06ce0feccbcaf29af6aa582f7fc4",
+    "url": "/International-Model-UN-Website/static/media/tokyo.820e06ce.png"
+  },
+  {
+    "revision": "ce38d1e1de0c6a438e5d6ae43a412ab9",
+    "url": "/International-Model-UN-Website/static/media/washington.ce38d1e1.png"
+  }
+]);
