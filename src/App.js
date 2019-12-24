4af333ea73c315afacb2133d6dff2396dab4e98f
CosmicSage <prabhakarmishra9@yahoo.com>
2019-12-24 17:39:29 +0530

    Review Carousel Fix & RBoxShadow ToBe

--- src/App.js
+++ src/App.js
@@ -223,4 +223,57 @@ let styles = {
 <div className="carousel-item">
   <div className="d-block w-1oo carousel_slide"></div>
 </div>
+
+import React from "react";
+const Reviews = [
+  {img : require("../static/images/Sand Siwapron.png"), person : {name : "Sand Siwapron", nation : "Thailand"}, review : "This is my first Model United Nations conference where I have gained a lot of new experience and have met people who are smart, kind and hardworking. Most importantly, it made me want to improve myself more!"},
+  {img : require("../static/images/Sand Siwapron.png"), person : {name : "Sand Siwapron", nation : "Thailand"}, review : "This is my first Model United Nations conference where I have gained a lot of new experience and have met people who are smart, kind and hardworking. Most importantly, it made me want to improve myself more!"},
+  {img : require("../static/images/Sand Siwapron.png"), person : {name : "Sand Siwapron", nation : "Thailand"}, review : "This is my first Model United Nations conference where I have gained a lot of new experience and have met people who are smart, kind and hardworking. Most importantly, it made me want to improve myself more!"},
+];
+const Review = props => (
+  <div className="carousel-item ">
+    <div className="w-1oo carousel_slide">
+      <div className="review">
+        <span><img src={require("../static/images/quote.png")}/>{props.review}</span>
+      </div>
+      <div className="person">
+          <img src={props.img}/>
+          <span style={{fontWeight : "bold", fontSize : 18}}>{props.person.name}</span>
+          <span style={{fontSize : 16}}>{props.person.nation}</span>
+      </div>
+    </div>
+  </div>
+);
+export default function Carousel() {
+  return (
+    <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+      <ol className="carousel-indicators">
+        <li  data-target="#carouselExampleIndicators" data-slide-to="0" className="active invertt"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1" className="invertt"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2" className="invertt"></li>
+      </ol>
+      <div className="carousel-inner">
+        {Reviews.map(review => <Review img={review.img} person={review.person} review={review.review}/> )}
+      </div>
+      <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span className="carousel-control-prev-icon invertt" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
+      </a>
+      <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span className="carousel-control-next-icon invertt" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
+      </a>
+    </div>
+   );
+}
+
+
+// <img className="d-block w-100"  alt="First slide"/>
+
+// <div className="carousel-item">
+  // <div className="d-block w-1oo carousel_slide"></div>
+// </div>
+
+// <RoundArrowButton color="light" value="Know More" size="lg" arrow="./static/images/whitearrow.png"/>
+
 **/
--- src/components/Review.js
+++ src/components/Review.js
@@ -1,44 +1,55 @@
 import React from "react";
-
+const Reviews = [
+  {img : require("../static/images/Sand Siwapron.png"), person : {name : "Sand Siwapron", nation : "Thailand"}, review : "This is my first Model United Nations conference where I have gained a lot of new experience and have met people who are smart, kind and hardworking. Most importantly, it made me want to improve myself more!"},
+  {img : require("../static/images/Sand Siwapron.png"), person : {name : "Sand Siwapron", nation : "Thailand"}, review : "This is my first Model United Nations conference where I have gained a lot of new experience and have met people who are smart, kind and hardworking. Most importantly, it made me want to improve myself more!"},
+  {img : require("../static/images/Sand Siwapron.png"), person : {name : "Sand Siwapron", nation : "Thailand"}, review : "This is my first Model United Nations conference where I have gained a lot of new experience and have met people who are smart, kind and hardworking. Most importantly, it made me want to improve myself more!"},
+  {img : require("../static/images/Sand Siwapron.png"), person : {name : "Sand Siwapron", nation : "Thailand"}, review : "This is my first Model United Nations conference where I have gained a lot of new experience and have met people who are smart, kind and hardworking. Most importantly, it made me want to improve myself more!"},
+];
+function Index(props) {
+  if (props.index === 0)
+    var activate = "active";
+  return <li  data-target="#carouselExampleIndicators" data-slide-to={props.index} className={`${activate} invertt`}></li>;
+}
+const Review = props => {
+  if (props.index === 0)
+    var activate = "active";
+  return (
+    <div className={`carousel-item ${activate}`} >
+      <div className="w-1oo carousel_slide">
+        <div className="review">
+          <span><img src={require("../static/images/quote.png")}/>{props.review}</span>
+        </div>
+        <div className="person">
+            <img src={props.img}/>
+            <span style={{fontWeight : "bold", fontSize : 18}}>{props.person.name}</span>
+            <span style={{fontSize : 16}}>{props.person.nation}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default function Carousel() {
   return (
     <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
       <ol className="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        {Reviews.map((r, i) => <Index index={i}/> )}
       </ol>
       <div className="carousel-inner">
-        <div className="carousel-item active">
-          <div className="d-block w-1oo carousel_slide"></div>
-        </div>
-        <div className="carousel-item ">
-          <div className="w-1oo carousel_slide">
-            <div className="review">
-              This is my first Model United Nations conference where I have gained a lot of new experience and have met people who are smart, kind and hardworking. Most importantly, it made me want to improve myself more!
-            </div>
-            <div className="person">
-                <img src={require("../static/images/Sand Siwapron.png")}/>
-                <span>Sand Siwapron</span>
-            </div>
-          </div>
-        </div>
+        {Reviews.map((review, index) => <Review img={review.img} person={review.person} review={review.review} index={index}/> )}
       </div>
       <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span className="carousel-control-prev-icon invert" aria-hidden="true"></span>
+        <span className="carousel-control-prev-icon invertt" aria-hidden="true"></span>
         <span className="sr-only">Previous</span>
       </a>
       <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span className="carousel-control-next-icon invert" aria-hidden="true"></span>
+        <span className="carousel-control-next-icon invertt" aria-hidden="true"></span>
         <span className="sr-only">Next</span>
       </a>
     </div>
    );
 }
 
-const Review = props => {
-  return null;
-}
+
 // <img className="d-block w-100"  alt="First slide"/>
 
 // <div className="carousel-item">
