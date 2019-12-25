e3e2b4774a874af15ea874020ea464ed9936a906
CosmicSage <prabhakarmishra9@yahoo.com>
2019-12-25 22:31:37 +0530

    FakeCommit(FC) 2 to changeLanguage see commit before'FC'1 for real code

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './static/css/index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-ReactDOM.render(<App />, document.getElementById('root'));
