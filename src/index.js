c3be83940dc64c01f4a13da24930f124290b9562
CosmicSage <prabhakarmishra9@yahoo.com>
2020-12-09 19:59:26 +0530

    React Router Init Complete

--- src/index.js
+++ src/index.js
@@ -3,4 +3,18 @@ import ReactDOM from 'react-dom';
 import './static/css/index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Pricing from "./Pricing";
+
+export default function Routes() {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/pricing" component={Pricing} />
+        <Route component={() => (<div>404 Not found </div>)} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+ReactDOM.render(<Routes />, document.getElementById('root'));
